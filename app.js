d47902c264e38290c160239c831cb4a5a03fb762
LyChristopherC <LyChristopherC@gmail.com>
2022-03-10 16:03:12 -0500

    We workin

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-const express = require("express")
-const morgan = require("morgan")
-const layout = require("./views/layout")
-const path = require("path")
+const express = require("express");
+const morgan = require("morgan");
+const layout = require("./views/layout");
+const path = require("path");
+const { db } = require("./models");
 
-const app = express()
-app.use(express.urlencoded({ extended: false }))
+const app = express();
+app.use(express.urlencoded({ extended: false }));
 
-app.use(morgan("dev"))
-app.use("/static", express.static(path.join(__dirname, "public")))
+app.use(morgan("dev"));
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res, next) => {
-	res.send(layout(""))
-})
+  res.send(layout(""));
+});
 
-app.listen(3000, () => {
-	console.log(`App listening in port ${3000}`)
-})
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
+
+const run = async () => {
+  await db.sync({ force: true });
+  app.listen(3000, () => {
+    console.log(`App listening in port ${3000}`);
+  });
+};
+
+run();
